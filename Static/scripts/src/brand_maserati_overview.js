7400672247be83a7b4309d3f535f0335619ad916
NH Evan <nhevan@gmail.com>
2017-08-01 20:50:15 +0600

    New Static folder from Harry - fetched from live

--- Static/scripts/src/brand_maserati_overview.js
+++ Static/scripts/src/brand_maserati_overview.js
@@ -1,49 +1,60 @@
 define(['jquery'], function ($) {
     function init(container) {
 
-        $('.brand-tabs__container a').click(function (e) {
-            e.preventDefault();
-
-            $(this).closest('.brand-tabs__container').find('a').removeClass('active').filter(this)
-                .addClass('active');
-
-            var pane = $(this).attr('href');
-            $('#brand_content_container .accessory-detail').hide().filter(pane).show();
-        });
-
-        $('.btn').click(function (e) {
-            e.preventDefault();
-
-            var $this = $(this),
-                currentLevel = $this.closest('.brand-content-group').data('level'),
-                target = $(this).attr('target'),
-                $target = $(target),
-                maxLevel = 10, // harcode max level of buttons can be displayed
-                normalBgColor = '#FD5204',
-                selectedBgColor = '#454545';
-
-            $('.brand-content-group-level' + currentLevel).find('.btn').css('background-color', normalBgColor);
-            $this.css('background-color', selectedBgColor);
-
-            if ($target) {
-                var targetLevel = $target.data('level') || ++currentLevel;
-                for (var i = targetLevel; i < maxLevel; i++) {
-                    $('.brand-content-group-level' + i).hide().find('.btn').css('background-color', normalBgColor);
-                }
-
-                if (!$this.hasClass('show-group'))
-                    $(this.getAttribute("target") + ' .modal-dialog').css('top', '50%');
-                else
-                    $target.show();
-
+        $('.first .btn').click(function() {
+            if (window.innerWidth > 900) {
+                $('.second, .third').hide();
+                $('.first .btn').css('background-color', '#FD5204');
+                $('.second .btn').css('background-color', '#002D6A');
+                $('.third .btn').css('background-color', '#170B0D');
+                $(this).css('background-color', '#454545');
+                $(this.getAttribute("target")).show();
+                console.log(this.getAttribute("target"));
+            } else {
+                $('.second, .third').hide();
+                $('.first .btn').css('background-color', '#FD5204');
+                $('.second .btn').css('background-color', '#002D6A');
+                $('.third .btn').css('background-color', '#170B0D');
+                $(this).css('background-color', '#454545');
+                $(this.getAttribute("target")).insertAfter(this);
+                $(this.getAttribute("target")).toggle();
+                console.log(this.getAttribute("target"));
             }
+
         });
 
-        $('.btn-close').click(function () {
+        $('.second .btn').click(function() {
+            if (window.innerWidth > 900) {
+                $('.third').hide();
+                $('.second .btn').css('background-color', '#002D6A');
+                $(this).css('background-color', '#454545');
+                $(this.getAttribute("target")).show();
+                console.log(this.getAttribute("target"));
+            } else {
+                $('.third').hide();
+                $('.second .btn').css('background-color', '#002D6A');
+                $(this).css('background-color', '#454545');
+                $(this.getAttribute("target")).insertAfter(this);
+                $(this.getAttribute("target")).toggle();
+                console.log(this.getAttribute("target"));
+            }
+
+        });
+
+        $('.third .btn').click(function() {
+            $('.third .btn').css('background-color', '#170B0D');
+            $(this).css('background-color', '#454545');
+            console.log(this);
+        });
+
+        $('.btn').click(function(){
+            $('.modal-dialog').css('top', '-200%');
+            $(this.getAttribute("data") + ' .modal-dialog').css('top', '50%');
+        });
+        $('.btn-close').click(function(){
             $(this).closest('.modal-dialog').css('top', '-200%');
         });
     }
-
     return {
         init: function (container) {
             init(container);
